530e8f4602201ea19c8689acc796175f86d8fe3d
viktor <ashenarx@gmail.com>
2025-11-05 01:30:16 +1000

    Add `KafkaConnect` to `flagforge-common-kafka` module

---
+++ modules/flagforge-common-kafka/src/dev/owlmajin/flagforge/server/common/kafka/KafkaConnect.kt
@@ -0,0 +1,30 @@
+package dev.owlmajin.flagforge.server.common.kafka
+
+import org.apache.kafka.clients.admin.AdminClient
+import org.apache.kafka.clients.admin.AdminClientConfig
+import org.apache.kafka.clients.admin.KafkaAdminClient
+import org.apache.kafka.clients.admin.NewTopic
+import org.springframework.boot.kafka.autoconfigure.KafkaProperties
+import org.springframework.kafka.core.KafkaAdmin
+import kotlin.uuid.Uuid
+
+private const val KAFKA_ADMIN = "kafka-admin"
+private const val KAFKA_ADMIN_CLIENT = "kafka-admin-client"
+
+class KafkaConnect(val kafkaProperties: KafkaProperties, val createTopicNames: Set<String>) {
+
+    val kafkaAdmin: KafkaAdmin = KafkaAdmin(kafkaProperties.buildAdminProperties().also {
+            it[AdminClientConfig.CLIENT_ID_CONFIG] = "${it[AdminClientConfig.CLIENT_ID_CONFIG]}-$KAFKA_ADMIN-${Uuid.random()}"
+        }).apply {
+            setAutoCreate(false)
+            setCreateOrModifyTopic { it.name() in createTopicNames }
+        }
+
+    val adminClient: AdminClient = KafkaAdminClient.create(kafkaProperties.buildAdminProperties().also {
+        it[AdminClientConfig.CLIENT_ID_CONFIG] = "${it[AdminClientConfig.CLIENT_ID_CONFIG]}-$KAFKA_ADMIN_CLIENT-${Uuid.random()}"
+    })
+
+    internal fun createOrModifyTopic(topics: Set<NewTopic>) {
+        kafkaAdmin.createOrModifyTopics(*topics.toTypedArray())
+    }
+}
