b81f486c6000daa654048f9a8fe8f799367d2bd7
viktor <ashenarx@gmail.com>
2025-11-06 01:57:50 +1000

    Add `flagforge-common-kafka` module to bootstrap and update Kafka configurations

--- bootstrap/src/dev/owlmajin/flagforge/server/bootstrap/Application.kt
+++ bootstrap/src/dev/owlmajin/flagforge/server/bootstrap/Application.kt
@@ -3,7 +3,7 @@ package dev.owlmajin.flagforge.server.bootstrap
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = ["dev.owlmajin.flagforge.server"])
 class Application
 
 fun main(args: Array<String>) {
