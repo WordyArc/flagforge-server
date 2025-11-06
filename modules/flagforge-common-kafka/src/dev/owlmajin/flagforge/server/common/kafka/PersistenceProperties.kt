74d00b169b61ed7db40bf292635ec9669750b7ce
viktor <ashenarx@gmail.com>
2025-11-06 22:47:46 +1000

    Refactor `common-kafka`: replace schema URLs with standardized URN

--- modules/flagforge-common-kafka/src/dev/owlmajin/flagforge/server/common/kafka/PersistenceProperties.kt
+++ modules/flagforge-common-kafka/src/dev/owlmajin/flagforge/server/common/kafka/PersistenceProperties.kt
@@ -3,8 +3,9 @@ package dev.owlmajin.flagforge.server.common.kafka
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.kafka.autoconfigure.KafkaProperties
 
-const val DATA_SCHEMA_RUNTIME_DATA: String = "https://flagforge.dev/event/runtime/1.0.0"
-const val DATA_SCHEMA_HISTORIC_DATA: String = "https://flagforge.dev/event/historic/1.0.0"
+const val DATA_SCHEMA_HEADER_NAME = "data-schema"
+const val DATA_SCHEMA_RUNTIME_V1_0_0 = "urn:flagforge:event:runtime:1.0.0"
+const val DATA_SCHEMA_HISTORIC_V1_0_0 = "urn:flagforge:event:historic:1.0.0"
 
 private const val DELETE_POLICY = "delete"
 private const val COMPACT_POLICY = "compact"
@@ -49,43 +50,43 @@ class PersistenceProperties {
             "flag-commands",
             "flagCommandsTopic",
             DELETE_POLICY,
-            DATA_SCHEMA_RUNTIME_DATA
+            DATA_SCHEMA_RUNTIME_V1_0_0
         )
         flagEvents = flagEvents.withDefaults(
             "flag-events",
             "flagEventsTopic",
             DELETE_POLICY,
-            DATA_SCHEMA_RUNTIME_DATA
+            DATA_SCHEMA_RUNTIME_V1_0_0
         )
         flagState = flagState.withDefaults(
             "flag-state",
             "flagStateTopic",
             COMPACT_POLICY,
-            DATA_SCHEMA_RUNTIME_DATA
+            DATA_SCHEMA_RUNTIME_V1_0_0
         )
         segmentState = segmentState.withDefaults(
             "segment-state",
             "segmentStateTopic",
             COMPACT_POLICY,
-            DATA_SCHEMA_RUNTIME_DATA
+            DATA_SCHEMA_RUNTIME_V1_0_0
         )
         envState = envState.withDefaults(
             "env-state",
             "envStateTopic",
             COMPACT_POLICY,
-            DATA_SCHEMA_RUNTIME_DATA
+            DATA_SCHEMA_RUNTIME_V1_0_0
         )
         projectState = projectState.withDefaults(
             "project-state",
             "projectStateTopic",
             COMPACT_POLICY,
-            DATA_SCHEMA_RUNTIME_DATA
+            DATA_SCHEMA_RUNTIME_V1_0_0
         )
         sdkKeys = sdkKeys.withDefaults(
             "sdk-keys",
             "sdkKeysTopic",
             COMPACT_POLICY,
-            DATA_SCHEMA_RUNTIME_DATA
+            DATA_SCHEMA_RUNTIME_V1_0_0
         )
     }
 }
