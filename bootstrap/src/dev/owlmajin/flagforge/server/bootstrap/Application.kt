3bbb82619b25ff8e0bc99f940aefa2a4b53d214b
viktor <ashenarx@gmail.com>
2025-11-09 00:02:54 +1000

    Refactor `bootstrap` module: update dependencies, configuration, and import `ControlApiConfiguration`

--- bootstrap/src/dev/owlmajin/flagforge/server/bootstrap/Application.kt
+++ bootstrap/src/dev/owlmajin/flagforge/server/bootstrap/Application.kt
@@ -1,9 +1,18 @@
 package dev.owlmajin.flagforge.server.bootstrap
 
+import dev.owlmajin.flagforge.server.control.api.config.ControlApiConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.gateway.mvc.config.ProxyResponseAutoConfiguration
+import org.springframework.context.annotation.Import
 
-@SpringBootApplication(scanBasePackages = ["dev.owlmajin.flagforge.server"])
+@Import(ControlApiConfiguration::class)
+@SpringBootApplication(
+    exclude = [
+        ProxyResponseAutoConfiguration::class
+    ]
+)
+//@SpringBootApplication(scanBasePackages = ["dev.owlmajin.flagforge.server"])
 class Application
 
 fun main(args: Array<String>) {
---
+++ bootstrap/src/dev/owlmajin/flagforge/server/bootstrap/config/BootstrapConfiguration.kt
@@ -0,0 +1,8 @@
+package dev.owlmajin.flagforge.server.bootstrap.config
+
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+
+//@Configuration
+//@ComponentScan(basePackages = ["dev.owlmajin.flagforge.server.bootstrap"])
+class BootstrapConfiguration
