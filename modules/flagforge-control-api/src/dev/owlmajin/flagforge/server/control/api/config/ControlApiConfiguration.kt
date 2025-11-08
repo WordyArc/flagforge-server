14a8645167ca70b948fecc2990305c8dc0a9f174
viktor <ashenarx@gmail.com>
2025-11-09 02:07:00 +1000

    Add Jackson ObjectMapper configuration to `control-api`

--- modules/flagforge-control-api/src/dev/owlmajin/flagforge/server/control/api/config/ControlApiConfiguration.kt
+++ modules/flagforge-control-api/src/dev/owlmajin/flagforge/server/control/api/config/ControlApiConfiguration.kt
@@ -3,9 +3,14 @@ package dev.owlmajin.flagforge.server.control.api.config
 import dev.owlmajin.flagforge.server.common.CommonConfiguration
 import dev.owlmajin.flagforge.server.common.kafka.config.CommonKafkaConfiguration
 import dev.owlmajin.flagforge.server.persistence.config.PersistenceConfiguration
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Import
 import org.springframework.context.annotation.Profile
+import tools.jackson.core.StreamReadFeature
+import tools.jackson.databind.DeserializationFeature
+import tools.jackson.databind.ObjectMapper
+import tools.jackson.databind.json.JsonMapper
 
 @Import(value = [
     CommonConfiguration::class,
@@ -14,4 +19,14 @@ import org.springframework.context.annotation.Profile
 ])
 @Profile("control-api")
 @ComponentScan(basePackages = ["dev.owlmajin.flagforge.server.control.api"])
-class ControlApiConfiguration
+class ControlApiConfiguration {
+
+    @Bean
+    fun jacksonBuilder(): ObjectMapper =
+        JsonMapper.builder()
+            .enable(DeserializationFeature.USE_BIG_DECIMAL_FOR_FLOATS)
+            .enable(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES)
+            .enable(StreamReadFeature.INCLUDE_SOURCE_IN_LOCATION)
+            .build()
+
+}
