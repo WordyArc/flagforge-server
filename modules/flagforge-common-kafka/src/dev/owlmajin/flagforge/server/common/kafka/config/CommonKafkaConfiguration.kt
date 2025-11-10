aa6cee2a26543d2cf15de089728f9b1eea749533
viktor <ashenarx@gmail.com>
2025-11-11 01:47:34 +1000

    Remove `KafkaPayloadEncoder` and related Jackson encoder setup

--- modules/flagforge-common-kafka/src/dev/owlmajin/flagforge/server/common/kafka/config/CommonKafkaConfiguration.kt
+++ modules/flagforge-common-kafka/src/dev/owlmajin/flagforge/server/common/kafka/config/CommonKafkaConfiguration.kt
@@ -1,8 +1,6 @@
 package dev.owlmajin.flagforge.server.common.kafka.config
 
 import dev.owlmajin.flagforge.server.common.kafka.producer.TopicProducerFactory
-import dev.owlmajin.flagforge.server.common.kafka.serde.JacksonPayloadEncoder
-import dev.owlmajin.flagforge.server.common.kafka.serde.KafkaPayloadEncoder
 import dev.owlmajin.flagforge.server.common.kafka.topic.KafkaConnect
 import dev.owlmajin.flagforge.server.common.kafka.topic.PersistenceProperties
 import org.springframework.boot.context.properties.EnableConfigurationProperties
@@ -10,7 +8,6 @@ import org.springframework.boot.kafka.autoconfigure.KafkaProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
-import tools.jackson.databind.json.JsonMapper
 
 @Configuration
 @EnableConfigurationProperties(PersistenceProperties::class)
@@ -20,8 +17,6 @@ class CommonKafkaConfiguration {
     @Bean(destroyMethod = "close")
     fun kafkaConnect(persistenceProperties: PersistenceProperties) = KafkaConnect(persistenceProperties.kafka)
 
-    @Bean
-    fun kafkaPayloadEncoder(jsonMapper: JsonMapper): KafkaPayloadEncoder = JacksonPayloadEncoder(jsonMapper)
 
     @Bean(destroyMethod = "close")
     fun topicProducerFactory(kafkaProperties: KafkaProperties) = TopicProducerFactory(kafkaProperties)
