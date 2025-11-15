03b42ceae4297dea5ecfd71202a848ccc69a2d77
viktor <ashenarx@gmail.com>
2025-11-16 04:47:45 +1000

    Add `flagforge-evaluation-api` module

---
+++ modules/flagforge-evaluation-api/src/dev/owlmajin/flagforge/server/evaluation/api/config/EvaluationApiConfiguration.kt
@@ -0,0 +1,8 @@
+package dev.owlmajin.flagforge.server.evaluation.api.config
+
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@ComponentScan("dev.owlmajin.flagforge.server.evaluation.api")
+class EvaluationApiConfiguration
