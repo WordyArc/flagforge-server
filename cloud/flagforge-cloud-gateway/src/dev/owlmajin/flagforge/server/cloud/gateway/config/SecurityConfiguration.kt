b0afd717b6adbfbf004b8eebc10f38eb0821621f
viktor <ashenarx@gmail.com>
2025-11-26 06:05:36 +1000

    Remove OIDC Auth Proxy dependency and simplify `SecurityConfiguration` in `flagforge-cloud-gateway`

--- cloud/flagforge-cloud-gateway/src/dev/owlmajin/flagforge/server/cloud/gateway/config/SecurityConfiguration.kt
+++ cloud/flagforge-cloud-gateway/src/dev/owlmajin/flagforge/server/cloud/gateway/config/SecurityConfiguration.kt
@@ -1,6 +1,5 @@
 package dev.owlmajin.flagforge.server.cloud.gateway.config
 
-import dev.owlmajin.oidc.authproxy.spring.security.JwtAuthProxyConverter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity
@@ -12,7 +11,7 @@ import org.springframework.security.web.server.SecurityWebFilterChain
 @Configuration
 @EnableWebFluxSecurity
 @EnableReactiveMethodSecurity
-class SecurityConfiguration(private val jwtAuthProxyConverter: JwtAuthProxyConverter) {
+class SecurityConfiguration() {
 
 
     @Bean
@@ -26,14 +25,6 @@ class SecurityConfiguration(private val jwtAuthProxyConverter: JwtAuthProxyConve
                 it.pathMatchers("/api/v1/evaluation/**")
                     .hasAnyAuthority("flagforge-cloud-gateway_api_user", "flagforge-cloud-gateway_api_admin")
                 it.anyExchange().authenticated()
-            }
-            .oauth2ResourceServer { oauth2 ->
-                oauth2.jwt { jwt ->
-                    jwt.jwtAuthenticationConverter(
-                        ReactiveJwtAuthenticationConverterAdapter(jwtAuthProxyConverter)
-                    )
-                }
-            }
-            .build()
+            }.build()
 
 }
